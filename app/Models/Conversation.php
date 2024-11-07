e79be89308a5e1b56edae768ab4df6e96a496dcd
amirrezamcp <mousavi.amirreza30@gmail.com>
2024-11-07 12:22:59 +0330

    update models

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id1',
         'user_id2',
